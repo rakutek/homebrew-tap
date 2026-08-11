131d21dd1c937f00d8860692cb375256dbd4da10
Raku Sakaguchi <raku.sakaguchi@dely.jp>
2026-08-11 10:46:37 +0900

    wtx formulaの監査指摘を修正

--- Formula/wtx.rb
+++ Formula/wtx.rb
@@ -2,12 +2,11 @@ class Wtx < Formula
   desc "One Lima microVM with a dedicated dockerd per git worktree"
   homepage "https://github.com/rakutek/wtx"
   url "https://github.com/rakutek/wtx/releases/download/v0.6.0/wtx-0.6.0-aarch64-apple-darwin.tar.gz"
-  version "0.6.0"
   sha256 "7953111ff691e01efa7cfb0a816fe5983f0d3b58c6d86130bdb5a99f20df8136"
   license any_of: ["MIT", "Apache-2.0"]
 
-  depends_on :macos
   depends_on "lima"
+  depends_on :macos
 
   def install
     odie "wtx requires Apple Silicon" unless Hardware::CPU.arm?
